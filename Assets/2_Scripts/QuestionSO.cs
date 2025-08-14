415b37d0b9e6f753af6b04fe8b4bcec1e210be34
lsw-07 <vahyeon2505@sonline20.sen.go.kr>
2025-08-14 10:24:33 +0900

    스크립터블 오브젝트

---
+++ Assets/2_Scripts/QuestionSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2, 6)]
+    [SerializeField] string question = "여기에 질문을 적어주세요.";
+}
